4c67c182783f8a079b83688d6f6909fc50cd2fe6
jobbin <qiubinbin0615@gmail.com>
2019-04-02 19:05:03 +0900

    [Update] delete ACCOUNT_ID

--- front_fc.py
+++ front_fc.py
@@ -10,7 +10,6 @@ def handler(environ, start_response):
     logger.info(environ)
 
     msg_to_slack = None
-    ACCOUNT_ID = os.environ['ACCOUNT_ID']
     SLACK_TOKEN = os.environ['SLACK_TOKEN']
     SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
     FUNCTION_COMPUTE_ENDPOINT = os.environ['FUNCTION_COMPUTE_ENDPOINT']
@@ -43,16 +42,11 @@ def handler(environ, start_response):
         logger.error(msg_to_slack)
 
     if len(PARAMS) < 2 or len(PARAMS) > 3:
-        msg_to_slack = "[Err] Increted Params ! Please check your /ecs command !"
+        msg_to_slack = '[Err] Incorrect Params ! Please check your /ecs command !'
         logger.error(msg_to_slack)
 
     if ECS_CMD not in ECS_CMD_CODE:
-        _cmd_info = '[ '
-        for data in ECS_CMD_CODE:
-            _cmd_info += data + '  '
-        _cmd_info += ' ]'
-
-        msg_to_slack = '[Err] Increted command : /ecs ' + ECS_CMD + '\n You can only use *' + _cmd_info +'*'
+        msg_to_slack = '[Err] Incorrect command : /ecs ' + ECS_CMD + '\n You can only use *' + ECS_CMD_CODE +'*'
         logger.error(msg_to_slack)
 
     if msg_to_slack is not None:
@@ -83,7 +77,6 @@ def handler(environ, start_response):
     msg_to_slack = '[Info] Run /ecs '
     for data in PARAMS:
         msg_to_slack += data
-        msg_to_slack += ' !\n'
     msg_to_slack = bytes(msg_to_slack, 'utf-8')
 
     status = '200 OK'
